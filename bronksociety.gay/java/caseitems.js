fc1cd098436ff9b85ef7d7bbc10061b8efacae07
Coach.Sludge <muddycj99@gmail.com>
2025-04-14 09:18:30 +0200

    gamble test
    added gif and new test mode

--- bronksociety.gay/java/caseitems.js
+++ bronksociety.gay/java/caseitems.js
@@ -1,63 +1,450 @@
 const caseItems = [
-    {
-      name: "USP-S | Peashooter",
-      rarity: "rarity-common",
-      gif: "gifs/usp.gif",
-      stats: {
-        "Kills": 204,
-        "Headshot %": "29%",
-        "Condition": "Field-Tested"
-      }
-    },
-    {
-      name: "P250 | Yeehaw",
-      rarity: "rarity-blue",
-      gif: "gifs/p250.gif",
-      stats: {
-        "Kills": 91,
-        "Headshot %": "35%",
-        "Condition": "Well-Worn"
-      }
-    },
-    {
-      name: "AWP | Cowpoke",
-      rarity: "rarity-purple",
-      gif: "gifs/awp.gif",
-      stats: {
-        "Kills": 612,
-        "Headshot %": "10%",
-        "Condition": "Minimal Wear"
-      }
-    },
-    {
-      name: "Baby Cigarette Pack",
-      rarity: "rarity-pink",
-      gif: "images/baby.gif",
-      stats: {
-        "Kills": 420,
-        "Headshot %": "54%",
-        "Condition": "Factory New"
-      }
-    },
-    {
-      name: "M4A1 | Death Ranch",
-      rarity: "rarity-red",
-      gif: "images/deadcowboy.gif",
-      stats: {
-        "Kills": 357,
-        "Headshot %": "48%",
-        "Condition": "Battle-Scarred"
-      }
-    },
-    {
-      name: "Rated M Label",
-      rarity: "rarity-gold",
-      gif: "images/ratedm.gif",
-      stats: {
-        "Kills": 69,
-        "Stabs": 20,
-        "Condition": "Factory New"
-      }
+  // Coors | Banquet variants
+  {
+    name: "Coors | Banquet",
+    rarity: "rarity-red",
+    gif: "images/csgo/banq.gif",
+    stats: {
+      "Rarity": "RED",
+      "Kills": 420,
+      "Headshot %": "42%",
+      "Condition": "Dusty",
+      "Chance to crash into a Bass Pro Shop": "18%"
     }
-  ];
-  
+  },
+  {
+    name: "Coors | Banquet",
+    rarity: "rarity-purple",
+    gif: "images/csgo/banq.gif",
+    stats: {
+      "Rarity": "PURPLE",
+      "Kills": 723,
+      "Headshot %": "51%",
+      "Condition": "Bottle Shattered",
+      "Effect": "Cowboy strength for 30s"
+    }
+  },
+  {
+    name: "Coors | Banquet",
+    rarity: "rarity-gold",
+    gif: "images/csgo/banq.gif",
+    stats: {
+      "Rarity": "GOLD",
+      "Kills": 1100,
+      "Headshot %": "66%",
+      "Condition": "Factory Fresh",
+      "Effect": "Unlocks trucker rage mode"
+    }
+  },
+
+  // ZYN MAGA | Gold variants
+  {
+    name: "ZYN MAGA | Gold",
+    rarity: "rarity-red",
+    gif: "images/csgo/zynmagagold.gif",
+    stats: {
+      "Rarity": "RED",
+      "Kills": 300,
+      "Headshot %": "39%",
+      "Condition": "Half Packed",
+      "Effect": "Voice gets 2x louder"
+    }
+  },
+  {
+    name: "ZYN MAGA | Gold",
+    rarity: "rarity-purple",
+    gif: "images/csgo/zynmagagold.gif",
+    stats: {
+      "Rarity": "PURPLE",
+      "Kills": 580,
+      "Headshot %": "55%",
+      "Condition": "Buzzing",
+      "Effect": "Temporarily immune to criticism"
+    }
+  },
+  {
+    name: "ZYN MAGA | Gold",
+    rarity: "rarity-gold",
+    gif: "images/csgo/zynmagagold.gif",
+    stats: {
+      "Rarity": "GOLD",
+      "Kills": 1000,
+      "Headshot %": "70%",
+      "Condition": "Legendary Spit",
+      "Effect": "Grants +5 patriotism"
+    }
+  },
+
+  // ZYN MAGA | Black variants
+  {
+    name: "ZYN MAGA | Black",
+    rarity: "rarity-red",
+    gif: "images/csgo/zynmagablack.gif",
+    stats: {
+      "Rarity": "RED",
+      "Kills": 320,
+      "Headshot %": "35%",
+      "Condition": "Scuffed",
+      "Effect": "+10 denial of wrongdoing"
+    }
+  },
+  {
+    name: "ZYN MAGA | Black",
+    rarity: "rarity-purple",
+    gif: "images/csgo/zynmagablack.gif",
+    stats: {
+      "Rarity": "PURPLE",
+      "Kills": 690,
+      "Headshot %": "59%",
+      "Condition": "Buzzcut",
+      "Effect": "Crowd control +2"
+    }
+  },
+  {
+    name: "ZYN MAGA | Black",
+    rarity: "rarity-gold",
+    gif: "images/csgo/zynmagablack.gif",
+    stats: {
+      "Rarity": "GOLD",
+      "Kills": 1111,
+      "Headshot %": "81%",
+      "Condition": "Executive Grade",
+      "Effect": "Temporarily immune to bans"
+    }
+  },
+
+  // ZYN LADEN | Gold variants
+  {
+    name: "ZYN LADEN | Gold",
+    rarity: "rarity-red",
+    gif: "images/csgo/zynladgold.gif",
+    stats: {
+      "Rarity": "RED",
+      "Kills": 333,
+      "Headshot %": "44%",
+      "Condition": "Battle-Scarred",
+      "Effect": "Summons whispers of jihad"
+    }
+  },
+  {
+    name: "ZYN LADEN | Gold",
+    rarity: "rarity-purple",
+    gif: "images/csgo/zynladgold.gif",
+    stats: {
+      "Rarity": "PURPLE",
+      "Kills": 666,
+      "Headshot %": "66%",
+      "Condition": "Polished",
+      "Effect": "+50% reload speed in caves"
+    }
+  },
+  {
+    name: "ZYN LADEN | Gold",
+    rarity: "rarity-gold",
+    gif: "images/csgo/zynladgold.gif",
+    stats: {
+      "Rarity": "GOLD",
+      "Kills": 999,
+      "Headshot %": "90%",
+      "Condition": "Bin Laden Certified",
+      "Effect": "Triggers automatic drone strike"
+    }
+  },
+
+  // ZYN LADEN | Black variants
+  {
+    name: "ZYN LADEN | Black",
+    rarity: "rarity-red",
+    gif: "images/csgo/zynladblack.gif",
+    stats: {
+      "Rarity": "RED",
+      "Kills": 420,
+      "Headshot %": "58%",
+      "Condition": "Terror Scarred",
+      "Effect": "Chants echo with every reload"
+    }
+  },
+  {
+    name: "ZYN LADEN | Black",
+    rarity: "rarity-purple",
+    gif: "images/csgo/zynladblack.gif",
+    stats: {
+      "Rarity": "PURPLE",
+      "Kills": 780,
+      "Headshot %": "71%",
+      "Condition": "Buried Deep",
+      "Effect": "Immune to UAV scans"
+    }
+  },
+  {
+    name: "ZYN LADEN | Black",
+    rarity: "rarity-gold",
+    gif: "images/csgo/zynladblack.gif",
+    stats: {
+      "Rarity": "GOLD",
+      "Kills": 1300,
+      "Headshot %": "93%",
+      "Condition": "Legendary",
+      "Effect": "May trigger Homeland Security alert"
+    }
+  },
+    {
+    name: "Marlboro | Red 100s",
+    rarity: "rarity-red",
+    gif: "images/csgo/mred.gif",
+    stats: {
+      "Rarity": "RED",
+      "Kills": 312,
+      "Headshot %": "43%",
+      "Condition": "Crushed",
+      "Effect": "+10 grit"
+    }
+  },
+  {
+    name: "Marlboro | Red 100s",
+    rarity: "rarity-purple",
+    gif: "images/csgo/mred.gif",
+    stats: {
+      "Rarity": "PURPLE",
+      "Kills": 601,
+      "Headshot %": "58%",
+      "Condition": "Smooth",
+      "Effect": "+20 smoker charm"
+    }
+  },
+  {
+    name: "Marlboro | Red 100s",
+    rarity: "rarity-gold",
+    gif: "images/csgo/mred.gif",
+    stats: {
+      "Rarity": "GOLD",
+      "Kills": 902,
+      "Headshot %": "71%",
+      "Condition": "Pristine",
+      "Effect": "Summons cowboy aura"
+    }
+  },
+
+  // Keystone | Light variants
+  {
+    name: "Keystone | Light",
+    rarity: "rarity-red",
+    gif: "images/csgo/keystone.gif",
+    stats: {
+      "Rarity": "RED",
+      "Kills": 380,
+      "Headshot %": "40%",
+      "Condition": "Flat",
+      "Effect": "Minor liver damage"
+    }
+  },
+  {
+    name: "Keystone | Light",
+    rarity: "rarity-purple",
+    gif: "images/csgo/keystone.gif",
+    stats: {
+      "Rarity": "PURPLE",
+      "Kills": 702,
+      "Headshot %": "62%",
+      "Condition": "Chilled",
+      "Effect": "Boosts beer pong accuracy"
+    }
+  },
+  {
+    name: "Keystone | Light",
+    rarity: "rarity-gold",
+    gif: "images/csgo/keystone.gif",
+    stats: {
+      "Rarity": "GOLD",
+      "Kills": 1200,
+      "Headshot %": "78%",
+      "Condition": "Blessed",
+      "Effect": "Enables truck flip on command"
+    }
+  },
+
+  // Guinness | Bottle variants
+  {
+    name: "Guinness | Bottle",
+    rarity: "rarity-red",
+    gif: "images/csgo/ginnis.gif",
+    stats: {
+      "Rarity": "RED",
+      "Kills": 390,
+      "Headshot %": "39%",
+      "Condition": "Imported",
+      "Effect": "+5 Irish luck"
+    }
+  },
+  {
+    name: "Guinness | Bottle",
+    rarity: "rarity-purple",
+    gif: "images/csgo/ginnis.gif",
+    stats: {
+      "Rarity": "PURPLE",
+      "Kills": 690,
+      "Headshot %": "55%",
+      "Condition": "Rich",
+      "Effect": "Voice gets 20% thicker"
+    }
+  },
+  {
+    name: "Guinness | Bottle",
+    rarity: "rarity-gold",
+    gif: "images/csgo/ginnis.gif",
+    stats: {
+      "Rarity": "GOLD",
+      "Kills": 1050,
+      "Headshot %": "80%",
+      "Condition": "Velvet",
+      "Effect": "Summons Riverdance distraction"
+    }
+  },
+  {
+    name: "Tesco | PIWO",
+    rarity: "rarity-common",
+    gif: "images/csgo/piwo.gif",
+    stats: {
+      "Rarity": "COMMON",
+      "Kills": 204,
+      "Headshot %": "29%",
+      "Condition": "Field-Tested"
+    }
+  },
+  {
+    name: "Tesco | PIWO",
+    rarity: "rarity-purple",
+    gif: "images/csgo/piwo.gif",
+    stats: {
+      "Rarity": "PURPLE",
+      "Kills": 764,
+      "Headshot %": "41%",
+      "Condition": "Battle-Scarred",
+      "Chance to blackout in public": "23%"
+    }
+  },
+  {
+    name: "Tesco | PIWO",
+    rarity: "rarity-gold",
+    gif: "images/csgo/piwo.gif",
+    stats: {
+      "Rarity": "GOLD",
+      "Kills": 1523,
+      "Headshot %": "60%",
+      "Condition": "Factory New",
+      "Chance to kill family of 4 while drunk driving": "12%"
+    }
+  },
+
+  // GMOD | Toolgun variants
+  {
+    name: "GMOD | Toolgun",
+    rarity: "rarity-common",
+    gif: "images/csgo/toolgun.gif",
+    stats: {
+      "Rarity": "COMMON",
+      "Kills": 394,
+      "Headshot %": "29%",
+      "Condition": "Field-Tested"
+    }
+  },
+  {
+    name: "GMOD | Toolgun",
+    rarity: "rarity-blue",
+    gif: "images/csgo/toolgun.gif",
+    stats: {
+      "Rarity": "BLUE",
+      "Kills": 678,
+      "Headshot %": "39%",
+      "Condition": "Squeaky",
+      "Effect": "+15 physics manipulation range"
+    }
+  },
+  {
+    name: "GMOD | Toolgun",
+    rarity: "rarity-purple",
+    gif: "images/csgo/toolgun.gif",
+    stats: {
+      "Rarity": "PURPLE",
+      "Kills": 1023,
+      "Headshot %": "62%",
+      "Condition": "Experimental",
+      "Effect": "Can freeze players permanently"
+    }
+  },
+
+  // White Monster | Ultra White Zero variants
+  {
+    name: "White Monster | Pack",
+    rarity: "rarity-red",
+    gif: "images/csgo/whitemonstercig.gif",
+    stats: {
+      "Rarity": "RED",
+      "Kills": 357,
+      "Headshot %": "48%",
+      "Condition": "Battle-Scarred"
+    }
+  },
+  {
+    name: "White Monster | Pack",
+    rarity: "rarity-purple",
+    gif: "images/csgo/whitemonstercig.gif",
+    stats: {
+      "Rarity": "PURPLE",
+      "Kills": 801,
+      "Headshot %": "66%",
+      "Condition": "Carbonated",
+      "Effect": "+25% energy regen"
+    }
+  },
+  {
+    name: "White Monster | Ultra White Zero",
+    rarity: "rarity-gold",
+    gif: "images/csgo/whitemonster.gif",
+    stats: {
+      "Rarity": "GOLD",
+      "Kills": 1337,
+      "Stabs": 20,
+      "Headshot %": "88%",
+      "Condition": "Factory New",
+      "Effect": "Auto-play alpha male podcasts nearby"
+    }
+  },
+
+  // Baby | Pack variants
+  {
+    name: "Baby | Pack",
+    rarity: "rarity-blue",
+    gif: "images/csgo/baby.gif",
+    stats: {
+      "Rarity": "BLUE",
+      "Kills": 91,
+      "Headshot %": "35%",
+      "Condition": "Well-Worn"
+    }
+  },
+  {
+    name: "Baby | Pack",
+    rarity: "rarity-pink",
+    gif: "images/csgo/baby.gif",
+    stats: {
+      "Rarity": "PINK",
+      "Kills": 666,
+      "Headshot %": "77%",
+      "Condition": "Factory New",
+      "Effect": "Secondhand Smoke Damage Radius +12%"
+    }
+  },
+  {
+    name: "Baby | Pack",
+    rarity: "rarity-gold",
+    gif: "images/csgo/baby.gif",
+    stats: {
+      "Rarity": "GOLD",
+      "Kills": 1243,
+      "Headshot %": "66%",
+      "Condition": "Shiny",
+      "Effect": "Immune to infant mortality"
+    }
+  }
+];
